9e0d32a13e529df7201cf56b4458980a6ed8f4f8
Robert812218 <robert812@shaw.ca>
2022-03-23 17:18:03 +0000

    add serialized data

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -33,78 +33,88 @@ $(document).ready(function() {
     }
   ]
   
-  const renderTweets = function(tweets) {
-  // loops through tweets
-  // calls createTweetElement for each tweet
-  // takes return value and appends it to the tweets container
-    //console.log("We are in the render Tweets");
-    for (const twt of tweets) {
-      let $tweet = createTweetElement(twt);
-      $('.other-tweets').append($tweet);
-    }
-  }
+  
   // fix this
   const createTweetElement = function(tweet) {
-
-    let $heartIcon = $("#heart-icon")
-
-    let $tweet = $(`<article class="old-tweet">
+    let $top = $(`
       <header>
         <div class="thumbnail">${tweet.user.name}</div>
         <div><img src="${tweet.user.avatars}"></img></div>
         <div class="handle">${tweet.user.handle}</div>
       </header>
-    <content>
-      <div>${tweet.content.text}</div>
-      <div>${tweet.created_at}</div>
-    </content>
-    <footer>
-      <div><i class="fa-solid fa-heart"></i></div>
-      <div><i class="fa-solid fa-flag"></i></div>
-      <div><i class="fa-solid fa-retweet"></i></div>
-    </footer>
-    </article>`);
+    `);
+
+    let $body = $(`
+      <content>
+        <div>${tweet.content.text}</div>
+        <div>${tweet.created_at}</div>
+      </content>
+    `);
+
+    let $bottom = $(`
+      <footer>
+        <div><i class="fa-solid fa-heart"></i></div>
+        <div><i class="fa-solid fa-flag"></i></div>
+        <div><i class="fa-solid fa-retweet"></i></div>
+      </footer>
+    `)
+  
+    let $tweet = $(`<article class="old-tweet"></article>`);
+    $tweet.append($top, $body, $bottom);
   
     return $tweet;
   }
-  
+
+  const renderTweets = function(tweets) {
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+      //console.log("We are in the render Tweets");
+    $container = $('.other-tweets');
+    tweets.forEach(tweet => {
+      const $tweet = createTweetElement(tweet);
+      $container.prepend($tweet);
+    })
+    console.log("We are in Render Tweets");
+  }
+
   renderTweets(data);
+
+  const fetchTweets = () => {
+    $.ajax({
+      method: "GET",
+      dataType: "json",
+      url: "../index.html",
+      success: posts => {
+        console.log(posts)
+        renderTweets(posts)
+      },
+      error: error => console.log(error)
+    });
+  }
+
+  fetchTweets();
+
+  const form = $('#createPostForm');
+  form.on('submit', function(event) {
+
+    event.preventDefault();
+    console.log(event);
+    const serializedData = $(this).serialize();
+    console.log(serializedData);
+
+    $.post('../index.html', serializedData)
+    .then((resp) => {
+      console.log(resp);
+      fetchPosts();
+    });
+
+    this.reset();
+  });
+
+  console.log("Hello!");
 });
 
 // add an event listener that listens for the submit event
 // prevent the default behaviour of the submit event (data submission and page refresh)
 // create an AJAX POST request in client.js that sends the form data to the server.
-
-$(document).submit(function(event) {
-  event.preventDefault();
-
-  const createPost = (posts) => {
-    const $header = $(`
-    <header>
-        <div class="thumbnail">${post.user.name}</div>
-        <div><img src="${tweet.user.avatars}"></img></div>
-        <div class="handle">${post.user.handle}</div>
-      </header>
-    `);
-    const $content = $(`
-    <content>
-      <div>${tweet.content.text}</div>
-      <div>${tweet.created_at}</div>
-    </content>
-    `);
-    const $footer = $(`
-    <footer>
-      <div><i class="fa-solid fa-heart"></i></div>
-      <div><i class="fa-solid fa-flag"></i></div>
-      <div><i class="fa-solid fa-retweet"></i></div>
-    </footer>
-    `);
-    const $post = $('<article class="old-tweet"></article>');
-    $post.append($header, $content, $footer);
-    return $post;
-  }
-
-
-
-
-});
